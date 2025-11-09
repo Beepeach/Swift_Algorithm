12485a5e0007c779b96464342bd113788b590e04
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-11-09 20:05:12 +0900

    [#4] Feat: Float 사용하면 안되는 이유

---
+++ C/FloatExam/FloatExam/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  FloatExam
+//
+//  Created by JUNHEE JO on 11/9/25.
+//
+
+#include <stdio.h>
+
+int main() {
+    double x = 123.456;
+
+    printf("%lu\n", sizeof(123.456F));
+    printf("%lu\n", sizeof(123.456));
+
+
+    printf("%f\n", 123.456F); // 바로 오차 발생
+    printf("%f\n", 123.456);
+
+    return 0;
+}
