44b7a04b78869b411d2b2fc2a0c0e216607ee459
JunHee Jo <68368774+Beepeach@users.noreply.github.com>
2025-11-10 17:06:20 +0900

    [#4] Refact: main 파라미터 추가 및 불필요한 공백 제거

--- C/FloatExam/FloatExam/main.c
+++ C/FloatExam/FloatExam/main.c
@@ -7,12 +7,11 @@
 
 #include <stdio.h>
 
-int main() {
+int main(void) {
     double x = 123.456;
 
     printf("%lu\n", sizeof(123.456F));
     printf("%lu\n", sizeof(123.456));
-
 
     printf("%f\n", 123.456F); // 바로 오차 발생
     printf("%f\n", 123.456);
